abe3c8b016242c8a8fb8a165ea46b1b746f5f995
Allan Camargo <allan.cdev@gmail.com>
2023-04-05 18:46:59 -0300

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/Button";
+
+export function App() {
+  return (
+  <>
+    <Button color="danger"/>
+    <Button color="primary"/>
+    <Button color="secondary"/>
+    <Button color="success"/>
+  </>
+  )
+}
